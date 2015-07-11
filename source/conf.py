058a08f80dd2a253214a010782e09bf29fdfbda1
Shota TAKAHASHI <shotakaha@gmail.com>
2015-07-11 18:57:15 +0900

    Modify : Set latex_elements

--- source/conf.py
+++ source/conf.py
@@ -216,15 +216,17 @@ htmlhelp_basename = 'KumaROOTdoc'
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
+    'papersize' : 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
+    'pointsize': '12pt',
 
 # Additional stuff for the LaTeX preamble.
 #'preamble': '',
 
 # Latex figure (float) alignment
-#'figure_align': 'htbp',
+    'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
