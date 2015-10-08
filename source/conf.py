8a9ec603681812f65cf255f8fbe5da42c2aa26f1
Shota TAKAHASHI <shotakaha@gmail.com>
2015-10-08 17:14:55 +0900

    M : Changed HTML template

--- source/conf.py
+++ source/conf.py
@@ -21,7 +21,7 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-import sphinx_bootstrap_theme
+# import sphinx_bootstrap_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -227,7 +227,7 @@ html_theme_options = {
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
