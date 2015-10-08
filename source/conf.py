aea88cce3e2f0d8ff101a930cd4204add83f64da
Shota TAKAHASHI <shotakaha@gmail.com>
2015-10-08 17:21:38 +0900

    M : Changed HTML theme

--- source/conf.py
+++ source/conf.py
@@ -147,9 +147,9 @@ numfig_secnum_depth = 1
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'alabaster'
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 # html_theme = 'bootstrap'
-html_theme = 'basicstrap'
+# html_theme = 'basicstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
