3260b3f40796357bf47ffe45b36ffb4e1f82bb55
Shota TAKAHASHI <shotakaha@gmail.com>
2015-10-08 17:19:58 +0900

    M : Fixed error

--- source/conf.py
+++ source/conf.py
@@ -40,7 +40,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinxjp.themes.basicstrap',
+#    'sphinxjp.themes.basicstrap',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
