87c644e2344a94cdf993620a23a8d5da6b94b86e
shotakah (lama) <shotakah@post.kek.jp>
2022-12-26 09:11:10 +0900

    build(docs/deploy.sh): デプロイ用スクリプトを修正した

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-make latexpdf
+poetry run make latexpdf
 rsync -auvz build/latex/kumaroot.pdf source/_static/
-make html
+poetry run make html
 rsync --delete -auvz build/html/ mypage:~/www/kumaroot/
