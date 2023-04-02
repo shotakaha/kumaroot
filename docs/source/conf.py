b450d139d2da8d58281f2912c31aea4eff5acd76
Shota Takahashi (KEK) <shotakah@post.kek.jp>
2023-04-02 09:08:33 +0900

    bump: version 1.8.9 → 1.9.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,8 @@
 project = "KumaROOT"
 author = "Shota TAKAHASHI"
 copyright = "2015 - 2023, Shota TAKAHASHI"
-version = "1.8.9"
-release = "1.8.9"
+version = "1.9.0"
+release = "1.9.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
