b950b02c39d2aad6161b085eb013b6da870c8aac
Shota Takahashi <shotakaha@kmi.nagoya-u.ac.jp>
2024-03-16 15:06:34 +0900

    style(g4examples/B1/src/RunAction.cc): フォーマットを整えた

--- g4examples/B1/include/RunAction.hh
+++ g4examples/B1/include/RunAction.hh
@@ -45,23 +45,22 @@ class G4Run;
 namespace B1
 {
 
-class RunAction : public G4UserRunAction
-{
-  public:
-    RunAction();
-    ~RunAction() override = default;
+    class RunAction : public G4UserRunAction
+    {
+    public:
+        RunAction();
+        ~RunAction() override = default;
 
-    void BeginOfRunAction(const G4Run*) override;
-    void   EndOfRunAction(const G4Run*) override;
+        void BeginOfRunAction(const G4Run *) override;
+        void EndOfRunAction(const G4Run *) override;
 
-    void AddEdep (G4double edep);
+        void AddEdep(G4double edep);
 
-  private:
-    G4Accumulable<G4double> fEdep = 0.;
-    G4Accumulable<G4double> fEdep2 = 0.;
-};
+    private:
+        G4Accumulable<G4double> fEdep = 0.;
+        G4Accumulable<G4double> fEdep2 = 0.;
+    };
 
 }
 
 #endif
-
--- g4examples/B1/src/RunAction.cc
+++ g4examples/B1/src/RunAction.cc
@@ -43,114 +43,119 @@
 namespace B1
 {
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-RunAction::RunAction()
-{
-  // add new units for dose
-  //
-  const G4double milligray = 1.e-3*gray;
-  const G4double microgray = 1.e-6*gray;
-  const G4double nanogray  = 1.e-9*gray;
-  const G4double picogray  = 1.e-12*gray;
+    RunAction::RunAction()
+    {
+        // add new units for dose
+        //
+        const G4double milligray = 1.e-3 * gray;
+        const G4double microgray = 1.e-6 * gray;
+        const G4double nanogray = 1.e-9 * gray;
+        const G4double picogray = 1.e-12 * gray;
 
-  new G4UnitDefinition("milligray", "milliGy" , "Dose", milligray);
-  new G4UnitDefinition("microgray", "microGy" , "Dose", microgray);
-  new G4UnitDefinition("nanogray" , "nanoGy"  , "Dose", nanogray);
-  new G4UnitDefinition("picogray" , "picoGy"  , "Dose", picogray);
+        new G4UnitDefinition("milligray", "milliGy", "Dose", milligray);
+        new G4UnitDefinition("microgray", "microGy", "Dose", microgray);
+        new G4UnitDefinition("nanogray", "nanoGy", "Dose", nanogray);
+        new G4UnitDefinition("picogray", "picoGy", "Dose", picogray);
 
-  // Register accumulable to the accumulable manager
-  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
-  accumulableManager->RegisterAccumulable(fEdep);
-  accumulableManager->RegisterAccumulable(fEdep2);
-}
+        // Register accumulable to the accumulable manager
+        G4AccumulableManager *accumulableManager = G4AccumulableManager::Instance();
+        accumulableManager->RegisterAccumulable(fEdep);
+        accumulableManager->RegisterAccumulable(fEdep2);
+    }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-void RunAction::BeginOfRunAction(const G4Run*)
-{
-  // inform the runManager to save random number seed
-  G4RunManager::GetRunManager()->SetRandomNumberStore(false);
+    void RunAction::BeginOfRunAction(const G4Run *)
+    {
+        // inform the runManager to save random number seed
+        G4RunManager::GetRunManager()->SetRandomNumberStore(false);
 
-  // reset accumulables to their initial values
-  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
-  accumulableManager->Reset();
+        // reset accumulables to their initial values
+        G4AccumulableManager *accumulableManager = G4AccumulableManager::Instance();
+        accumulableManager->Reset();
+    }
 
-}
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    void RunAction::EndOfRunAction(const G4Run *run)
+    {
+        G4int nofEvents = run->GetNumberOfEvent();
+        if (nofEvents == 0)
+            return;
 
-void RunAction::EndOfRunAction(const G4Run* run)
-{
-  G4int nofEvents = run->GetNumberOfEvent();
-  if (nofEvents == 0) return;
+        // Merge accumulables
+        G4AccumulableManager *accumulableManager = G4AccumulableManager::Instance();
+        accumulableManager->Merge();
 
-  // Merge accumulables
-  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
-  accumulableManager->Merge();
+        // Compute dose = total energy deposit in a run and its variance
+        //
+        G4double edep = fEdep.GetValue();
+        G4double edep2 = fEdep2.GetValue();
 
-  // Compute dose = total energy deposit in a run and its variance
-  //
-  G4double edep  = fEdep.GetValue();
-  G4double edep2 = fEdep2.GetValue();
+        G4double rms = edep2 - edep * edep / nofEvents;
+        if (rms > 0.)
+            rms = std::sqrt(rms);
+        else
+            rms = 0.;
 
-  G4double rms = edep2 - edep*edep/nofEvents;
-  if (rms > 0.) rms = std::sqrt(rms); else rms = 0.;
+        const auto detConstruction = static_cast<const DetectorConstruction *>(
+            G4RunManager::GetRunManager()->GetUserDetectorConstruction());
+        G4double mass = detConstruction->GetScoringVolume()->GetMass();
+        G4double dose = edep / mass;
+        G4double rmsDose = rms / mass;
 
-  const auto detConstruction = static_cast<const DetectorConstruction*>(
-    G4RunManager::GetRunManager()->GetUserDetectorConstruction());
-  G4double mass = detConstruction->GetScoringVolume()->GetMass();
-  G4double dose = edep/mass;
-  G4double rmsDose = rms/mass;
+        // Run conditions
+        //  note: There is no primary generator action object for "master"
+        //        run manager for multi-threaded mode.
+        const auto generatorAction = static_cast<const PrimaryGeneratorAction *>(
+            G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction());
+        G4String runCondition;
+        if (generatorAction)
+        {
+            const G4ParticleGun *particleGun = generatorAction->GetParticleGun();
+            runCondition += particleGun->GetParticleDefinition()->GetParticleName();
+            runCondition += " of ";
+            G4double particleEnergy = particleGun->GetParticleEnergy();
+            runCondition += G4BestUnit(particleEnergy, "Energy");
+        }
 
-  // Run conditions
-  //  note: There is no primary generator action object for "master"
-  //        run manager for multi-threaded mode.
-  const auto generatorAction = static_cast<const PrimaryGeneratorAction*>(
-    G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction());
-  G4String runCondition;
-  if (generatorAction)
-  {
-    const G4ParticleGun* particleGun = generatorAction->GetParticleGun();
-    runCondition += particleGun->GetParticleDefinition()->GetParticleName();
-    runCondition += " of ";
-    G4double particleEnergy = particleGun->GetParticleEnergy();
-    runCondition += G4BestUnit(particleEnergy,"Energy");
-  }
+        // Print
+        //
+        if (IsMaster())
+        {
+            G4cout
+                << G4endl
+                << "--------------------End of Global Run-----------------------";
+        }
+        else
+        {
+            G4cout
+                << G4endl
+                << "--------------------End of Local Run------------------------";
+        }
 
-  // Print
-  //
-  if (IsMaster()) {
-    G4cout
-     << G4endl
-     << "--------------------End of Global Run-----------------------";
-  }
-  else {
-    G4cout
-     << G4endl
-     << "--------------------End of Local Run------------------------";
-  }
+        G4cout
+            << G4endl
+            << " The run consists of " << nofEvents << " " << runCondition
+            << G4endl
+            << " Cumulated dose per run, in scoring volume : "
+            << G4BestUnit(dose, "Dose") << " rms = " << G4BestUnit(rmsDose, "Dose")
+            << G4endl
+            << "------------------------------------------------------------"
+            << G4endl
+            << G4endl;
+    }
 
-  G4cout
-     << G4endl
-     << " The run consists of " << nofEvents << " "<< runCondition
-     << G4endl
-     << " Cumulated dose per run, in scoring volume : "
-     << G4BestUnit(dose,"Dose") << " rms = " << G4BestUnit(rmsDose,"Dose")
-     << G4endl
-     << "------------------------------------------------------------"
-     << G4endl
-     << G4endl;
-}
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    void RunAction::AddEdep(G4double edep)
+    {
+        fEdep += edep;
+        fEdep2 += edep * edep;
+    }
 
-void RunAction::AddEdep(G4double edep)
-{
-  fEdep  += edep;
-  fEdep2 += edep*edep;
-}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 }
