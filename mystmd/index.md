b889aadadbd35085b64c171be95cc7f606081f85
Shota Takahashi (KMI) <shotakaha@kmi.nagoya-u.ac.jp>
2024-10-03 13:25:17 +0900

    refactor(indexes): それぞれのインデックスを更新した
    - modified:   mystmd/index.md
    - modified:   notebooks/index.ipynb
    - modified:   scripts/index.py

--- mystmd/index.md
+++ mystmd/index.md
@@ -1,9 +1,11 @@
-
-
-
-+++
-
-+++
+---
+jupytext:
+  text_representation:
+    extension: .md
+    format_name: myst
+    format_version: 0.13
+    jupytext_version: 1.16.4
+---
 
 # Sandbox of KumaROOT
 
--- scripts/index.py
+++ scripts/index.py
@@ -1,7 +1,13 @@
-#
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.4
+# ---
 
-# +++
-#
 # # Sandbox of KumaROOT
 #
 # 各種パッケージの使いかたを、Jupyter Notebooksを使って確認するスペースです。
