5806e7199e04c8b07855a7f277e69dbdc045ebc5
Shota Takahashi (KMI) <shotakaha@kmi.nagoya-u.ac.jp>
2024-10-17 16:47:21 +0900

    test(notebooks/pexpect.ipynb): pexpectの使い方を確認した

---
+++ mystmd/pexpect.md
@@ -0,0 +1,14 @@
+---
+jupytext:
+  text_representation:
+    extension: .md
+    format_name: myst
+    format_version: 0.13
+    jupytext_version: 1.16.4
+---
+
+# pexpect
+
+- `pexpect`で対話処理を自動化する
+- `rsync`でリモートサーバーからローカルにデータを同期する
+- `ssh`ログインしてサーバー情報をファイルに出力する
---
+++ scripts/pexpect.py
@@ -0,0 +1,15 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.4
+# ---
+
+# # pexpect
+#
+# - `pexpect`で対話処理を自動化する
+# - `rsync`でリモートサーバーからローカルにデータを同期する
+# - `ssh`ログインしてサーバー情報をファイルに出力する
