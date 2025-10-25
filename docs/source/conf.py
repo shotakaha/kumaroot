b9c8e4553d381e3a342cc913234a24e6da7f7330
Shota Takahashi (KMI) <shotakaha@kmi.nagoya-u.ac.jp>
2025-10-26 06:37:40 +0900

    bump: version 1.44.3 → 1.45.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,8 @@
 project = "KumaROOT"
 author = "Shota TAKAHASHI"
 copyright = "2015 - 2025, Shota TAKAHASHI"
-version = "1.44.3"
-release = "1.44.3"
+version = "1.45.0"
+release = "1.45.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
